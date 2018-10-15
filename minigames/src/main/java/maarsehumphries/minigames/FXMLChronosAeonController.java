aa9893196b6131f82b15d685494e1f55ff2530cc
zacharym44 <zacharym44@BHSCUR149008ITW.kprdsb.schools.local>
2018-10-15 13:40:50 -0400

    Initialized ChronosAeonController

---
+++ minigames/src/main/java/maarsehumphries/minigames/FXMLChronosAeonController.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Zachary Maarse
+ * Date: Oct 15, 2018
+ * Purpose: 
+ */
+package maarsehumphries.minigames;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author zacharym44
+ */
+public class FXMLChronosAeonController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
