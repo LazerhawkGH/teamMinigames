b508b0ff1f38c911641b9ae4acc356bcfa39d2b4
zacharym44 <zacharym44@BHSCUR149008ITW.kprdsb.schools.local>
2018-10-24 14:25:45 -0400

    minor fixes

--- minigames/src/main/java/maarsehumphries/minigames/FXMLMainMenu.java
+++ minigames/src/main/java/maarsehumphries/minigames/FXMLMainMenu.java
@@ -3,8 +3,6 @@
  * Date: Oct 15, 2018
  * Purpose: Displays the main menu, allows the user to select the game of their choice
  */
-
-
 package maarsehumphries.minigames;
 
 import java.io.IOException;
@@ -22,7 +20,6 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class FXMLMainMenu implements Initializable {
-    
 
     @FXML
     private Label label;
@@ -34,38 +31,41 @@ public class FXMLMainMenu implements Initializable {
     private Button btnG3;
     @FXML
     private Button btnExit;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
-        if (btnExit.isArmed()){
-        System.exit(0);}
-        if (btnG1.isArmed()){        
-        Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/FXMLJumpDodge.fxml")); 
-        Scene home_page_scene = new Scene(home_page_parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.hide();
-        stage.setScene(home_page_scene); 
-        stage.setTitle("Jump Dodge");
-        stage.show(); 
-        home_page_scene.getRoot().requestFocus();
-        stage.setOnCloseRequest(e -> System.exit(0));}
-        if (btnG2.isArmed()){
-        Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/FXMLChronosAeon.fxml")); 
-        Scene home_page_scene = new Scene(home_page_parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.hide();
-        stage.setScene(home_page_scene); 
-        stage.setTitle("Chronos Aeon");
-        stage.show(); 
-        home_page_scene.getRoot().requestFocus();
-        stage.setOnCloseRequest(e -> System.exit(0));}
-        if (btnG3.isArmed()){
+        if (btnExit.isArmed()) {
+            System.exit(0);
+        }
+        if (btnG1.isArmed()) {
+            Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/FXMLJumpDodge.fxml"));
+            Scene home_page_scene = new Scene(home_page_parent);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.hide();
+            stage.setScene(home_page_scene);
+            stage.setTitle("Jump Dodge");
+            stage.show();
+            home_page_scene.getRoot().requestFocus();
+            stage.setOnCloseRequest(e -> System.exit(0));
+        }
+        if (btnG2.isArmed()) {
+            Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/FXMLChronosAeon.fxml"));
+            Scene home_page_scene = new Scene(home_page_parent);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.hide();
+            stage.setScene(home_page_scene);
+            stage.setTitle("Chronos Aeon");
+            stage.show();
+            home_page_scene.getRoot().requestFocus();
+            stage.setOnCloseRequest(e -> System.exit(0));
+        }
+        if (btnG3.isArmed()) {
         }
 
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
