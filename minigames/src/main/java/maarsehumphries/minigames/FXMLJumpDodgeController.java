846196b79a1e302fd582902f45ed61caf0b1e79f
zacharym44 <zacharym44@BHSCUR149008ITW.kprdsb.schools.local>
2018-10-26 16:18:45 -0400

    Formatting

--- minigames/src/main/java/maarsehumphries/minigames/FXMLJumpDodgeController.java
+++ minigames/src/main/java/maarsehumphries/minigames/FXMLJumpDodgeController.java
@@ -28,83 +28,92 @@ import javafx.util.Duration;
  */
 public class FXMLJumpDodgeController implements Initializable {
 
-    @FXML private ImageView imgB;
-  @FXML private ImageView imgG;
-  @FXML private ImageView imgO;
-  @FXML private Label lblD;
-  @FXML private ImageView imgT;
-  @FXML private Button btnGame;
-  int s = 0;
-  int m = 20;
-  int n = 4;
-  int e = 0;
-  MotionBlur mb = new MotionBlur();
-            
-    Image G= new Image(getClass().getResource("/Stop sign.png").toString());
+    @FXML
+    private ImageView imgB;
+    @FXML
+    private ImageView imgG;
+    @FXML
+    private ImageView imgO;
+    @FXML
+    private Label lblD;
+    @FXML
+    private ImageView imgT;
+    @FXML
+    private Button btnGame;
+    int s = 0;
+    int m = 20;
+    int n = 4;
+    int e = 0;
+    MotionBlur mb = new MotionBlur();
+
+    Image G = new Image(getClass().getResource("/Stop sign.png").toString());
     Image B = new Image(getClass().getResource("/link attack.png").toString());
     Timeline jump = new Timeline(new KeyFrame(Duration.millis(20), ae -> up()));
     Timeline obstacles = new Timeline(new KeyFrame(Duration.millis(10), ae -> move()));
-    
+
     private boolean c(ImageView block1, ImageView block2) {
-    return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
-}
-       
-   public void keyPressed(KeyEvent event){
-        if (event.getCode()== KeyCode.SPACE){              
-            if(c(imgB,imgG)){
-               imgB.setTranslateY(imgB.getTranslateY()-5);
-               m = 20;
-               jump.play();
-        }
-        }
-}
-    
-   @FXML public void Begin(ActionEvent event){
-    obstacles.setCycleCount(Timeline.INDEFINITE);
-    obstacles.play();       //starts various timers
-    btnGame.setDisable(true);
-    imgO.setEffect(mb);
-    n = 4;
-    e = 0;
+        return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
     }
-    
-        public void up() {
-        imgB.setTranslateY(imgB.getTranslateY()-m); // moves the user upwards
-        m--; // decreases the amount the user wil go up by, until eventually going down again.
-        if(c(imgB,imgG)){
-        jump.stop(); //stops timer if user collides with ground
+
+    public void keyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.SPACE) {
+            if (c(imgB, imgG)) {
+                imgB.setTranslateY(imgB.getTranslateY() - 5);
+                m = 20;
+                jump.play();
+            }
         }
     }
-        
-      public void move() {
-        s++;
-        lblD.setText("Score: "+ s);
-        imgO.setTranslateX(imgO.getTranslateX()-n);
-        if (c(imgO,imgB)){
-        obstacles.stop();
-        imgO.setEffect(null);
-        imgO.setTranslateX(1);
-        btnGame.setDisable(false);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION); //shows alert to tell of game over
-        alert.setTitle("Game Over!");
-        alert.setHeaderText(null); 
-        alert.setContentText("Your Score was "+s+"!");
-        alert.show();
-        s = 0;  
-        lblD.setText("Score: "+ s);}
-        if(c(imgT,imgO)){
-        imgO.setTranslateX(300);
-        e++;
-        if (e == 8 && n < 9 ){
-        n++;
+
+    @FXML
+    public void Begin(ActionEvent event) {
+        obstacles.setCycleCount(Timeline.INDEFINITE);
+        obstacles.play();       //starts various timers
+        btnGame.setDisable(true);
+        imgO.setEffect(mb);
+        n = 4;
         e = 0;
-        }
-        }}
-                
-      public void tim() {
-      lblD.setText("Score "+ s++); //updates time of survival
     }
-    
+
+    public void up() {
+        imgB.setTranslateY(imgB.getTranslateY() - m); // moves the user upwards
+        m--; // decreases the amount the user wil go up by, until eventually going down again.
+        if (c(imgB, imgG)) {
+            jump.stop(); //stops timer if user collides with ground
+        }
+    }
+
+    public void move() {
+        s++;
+        lblD.setText("Score: " + s);
+        imgO.setTranslateX(imgO.getTranslateX() - n);
+        if (c(imgO, imgB)) {
+            obstacles.stop();
+            imgO.setEffect(null);
+            imgO.setTranslateX(1);
+            btnGame.setDisable(false);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION); //shows alert to tell of game over
+            alert.setTitle("Game Over!");
+            alert.setHeaderText(null);
+            alert.setContentText("Your Score was " + s + "!");
+            alert.show();
+            s = 0;
+            lblD.setText("Score: " + s);
+        }
+        if (c(imgT, imgO)) {
+            imgO.setTranslateX(300);
+            e++;
+            if (e == 8 && n < 9) {
+                n++;
+                e = 0;
+            }
+        }
+    }
+
+    public void tim() {
+        lblD.setText("Score " + s++); //updates time of survival
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         imgB.setImage(B);
@@ -113,5 +122,5 @@ public class FXMLJumpDodgeController implements Initializable {
         jump.setCycleCount(Timeline.INDEFINITE);
         mb.setRadius(6);
         imgO.setTranslateX(300);
-    }    
+    }
 }
