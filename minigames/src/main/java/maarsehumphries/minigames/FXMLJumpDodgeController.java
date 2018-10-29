d468aa5f76146a09f1c59659b169cd8160c8af70
shayneh58 <shayneh58@BHSCUR149007ITW.kprdsb.schools.local>
2018-10-29 14:26:56 -0400

    minor changes

--- minigames/src/main/java/maarsehumphries/minigames/FXMLJumpDodgeController.java
+++ minigames/src/main/java/maarsehumphries/minigames/FXMLJumpDodgeController.java
@@ -5,6 +5,7 @@ Shayne Humphries
 package maarsehumphries.minigames;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -12,7 +13,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.effect.MotionBlur;
 import javafx.scene.image.Image;
@@ -20,6 +24,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.util.Duration;
+import static maarsehumphries.minigames.MainApp.*;
 
 /**
  * FXML Controller class
@@ -28,90 +33,115 @@ import javafx.util.Duration;
  */
 public class FXMLJumpDodgeController implements Initializable {
 
-    @FXML private ImageView imgB;
-  @FXML private ImageView imgG;
-  @FXML private ImageView imgO;
-  @FXML private Label lblD;
-  @FXML private ImageView imgT;
-  @FXML private Button btnGame;
-  int s = 0;
-  int m = 20;
-  int n = 4;
-  int e = 0;
-  MotionBlur mb = new MotionBlur();
-            
-    Image G= new Image(getClass().getResource("/Stop sign.png").toString());
+    @FXML
+    private ImageView imgB;
+    @FXML
+    private ImageView imgG;
+    @FXML
+    private ImageView imgO;
+    @FXML
+    private Label lblD;
+    @FXML
+    private ImageView imgT;
+    @FXML
+    private Button btnGame;
+    @FXML
+    private Label lblPoints;
+    int s = 0;
+    int m = 20;
+    int n = 4;
+    int e = 0;
+    int f = 0;
+    MotionBlur mb = new MotionBlur();
+
+    Image G = new Image(getClass().getResource("/Stop sign.png").toString());
     Image B = new Image(getClass().getResource("/link attack.png").toString());
     Timeline jump = new Timeline(new KeyFrame(Duration.millis(20), ae -> up()));
     Timeline obstacles = new Timeline(new KeyFrame(Duration.millis(10), ae -> move()));
-    
+
     private boolean c(ImageView block1, ImageView block2) {
-    return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
-}
-       
-   public void keyPressed(KeyEvent event){
-        if (event.getCode()== KeyCode.SPACE){              
-            if(c(imgB,imgG)){
-               imgB.setTranslateY(imgB.getTranslateY()-5);
-               m = 20;
-               jump.play();
-        }
-        }
-}
-    
-   @FXML public void Begin(ActionEvent event){
-    obstacles.setCycleCount(Timeline.INDEFINITE);
-    obstacles.play();       //starts various timers
-    btnGame.setDisable(true);
-    imgO.setEffect(mb);
-    n = 4;
-    e = 0;
+        return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
     }
-    
-        public void up() {
-        imgB.setTranslateY(imgB.getTranslateY()-m); // moves the user upwards
-        m--; // decreases the amount the user wil go up by, until eventually going down again.
-        if(c(imgB,imgG)){
-        jump.stop(); //stops timer if user collides with ground
+
+    public void keyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ESCAPE) {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Exiting to Main Menu");
+            alert.setHeaderText("Do you wish to return to the main menu?");
+            ButtonType btnYes = new ButtonType("Yes");
+            ButtonType btnNo = new ButtonType("No", ButtonData.CANCEL_CLOSE);
+            alert.getButtonTypes().setAll(btnYes, btnNo);
+            Optional<ButtonType> result = alert.showAndWait();
+            
+        }
+        if (event.getCode() == KeyCode.SPACE) {
+            if (c(imgB, imgG)) {
+                imgB.setTranslateY(imgB.getTranslateY() - 5);
+                m = 50;
+                jump.play();
+            }
         }
     }
-        
-      public void move() {
-        s++;
-        lblD.setText("Score: "+ s);
-        imgO.setTranslateX(imgO.getTranslateX()-n);
-        if (c(imgO,imgB)){
-        obstacles.stop();
-        imgO.setEffect(null);
-        imgO.setTranslateX(1);
-        btnGame.setDisable(false);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION); //shows alert to tell of game over
-        alert.setTitle("Game Over!");
-        alert.setHeaderText(null); 
-        alert.setContentText("Your Score was "+s+"!");
-        alert.show();
-        s = 0;  
-        lblD.setText("Score: "+ s);}
-        if(c(imgT,imgO)){
-        imgO.setTranslateX(300);
-        e++;
-        if (e == 8 && n < 9 ){
-        n++;
+
+    @FXML
+    public void Begin(ActionEvent event) {
+        obstacles.setCycleCount(Timeline.INDEFINITE);
+        obstacles.play();       //starts various timers
+        btnGame.setDisable(true);
+        imgO.setEffect(mb);
+        n = 4;
         e = 0;
-        }
-        }}
-                
-      public void tim() {
-      lblD.setText("Score "+ s++); //updates time of survival
     }
-    
+
+    public void up() {
+        imgB.setTranslateY(imgB.getTranslateY() - m); // moves the user upwards
+        m--; // decreases the amount the user wil go up by, until eventually going down again.
+        if (c(imgB, imgG)) {
+            jump.stop(); //stops timer if user collides with ground
+        }
+    }
+
+    public void move() {
+        f += n - 3;
+        if (f > 70) {
+            s++;
+        }
+        lblD.setText("Score: " + s);
+        imgO.setTranslateX(imgO.getTranslateX() - n);
+        if (c(imgO, imgB)) {
+            obstacles.stop();
+            imgO.setEffect(null);
+            imgO.setTranslateX(1);
+            btnGame.setDisable(false);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION); //shows alert to tell of game over
+            alert.setTitle("Game Over!");
+            alert.setHeaderText(null);
+            alert.setContentText("Your Score was " + s + "!");
+            alert.show();
+            lblPoints.setText("Points " + getPoints() + s);
+            s = 0;
+            lblD.setText("Score: 0");
+        }
+        if (c(imgT, imgO)) {
+            imgO.setTranslateX(300);
+            e++;
+            if (e == 8 && n < 9) {
+                n++;
+                e = 0;
+            }
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        setPoints(getPoints());
+        s = getPoints();
+        lblPoints.setText("Points: " + getPoints());
         imgB.setImage(B);
         imgG.setImage(G);
         imgT.setTranslateX(-100);
         jump.setCycleCount(Timeline.INDEFINITE);
         mb.setRadius(6);
         imgO.setTranslateX(300);
-    }    
+    }
 }
--- minigames/src/main/java/maarsehumphries/minigames/FXMLRhythmController.java
+++ minigames/src/main/java/maarsehumphries/minigames/FXMLRhythmController.java
@@ -16,127 +16,181 @@ import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import static maarsehumphries.minigames.MainApp.*;
 
 public class FXMLRhythmController implements Initializable {
-    
-    @FXML private Label lblTest;
-    @FXML private ImageView imgUser;
-    @FXML private ImageView imgU;
-    @FXML private Button btnStart;
-    @FXML private ImageView imgB;
-    
+
+    @FXML
+    private Label lblTest;
+    @FXML
+    private ImageView imgUser;
+    @FXML
+    private ImageView imgU;
+    @FXML
+    private Button btnStart;
+    @FXML
+    private ImageView imgB;
+
     Timeline approach = new Timeline(new KeyFrame(Duration.millis(15), ae -> move()));
     Timeline stop = new Timeline(new KeyFrame(Duration.millis(500), ae -> top()));
-    
-    
+
     int rand = 1;
-    int h = 4;
+    int h = 3;
     int s = 0;
-    
+    int success = 0;
+    int remain = 0;
+
     private boolean left = false;
     private boolean right = false;
     private boolean down = false;
     private boolean up = false;
     private boolean game = false;
-    
-     ArrayList<Integer> list = new ArrayList();
-    
+
+    ArrayList<Integer> list = new ArrayList();
+
     public void Begin(ActionEvent event) {
-        if(game == false){
-        choose(); 
-        approach.play();   
-        h = 4;
-    }}
-    
-    public void keyPressed(KeyEvent event) {
-      if (event.getCode()== KeyCode.LEFT){
-      left = true;}
-      if (event.getCode()== KeyCode.RIGHT){
-      right = true;}
-      if (event.getCode()== KeyCode.DOWN){
-      down = true;}
-      if(event.getCode()==KeyCode.UP){
-      up = true;}
+        if (game == false) {
+            choose();
+            approach.play();
+            h = 4;
+            remain = 30;
+        }
     }
-    
+
+    public void keyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.LEFT) {
+            left = true;
+        }
+        if (event.getCode() == KeyCode.RIGHT) {
+            right = true;
+        }
+        if (event.getCode() == KeyCode.DOWN) {
+            down = true;
+        }
+        if (event.getCode() == KeyCode.UP) {
+            up = true;
+        }
+        if (event.getCode() == KeyCode.ESCAPE) {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Exiting to Main Menu");
+            alert.setHeaderText("Do you wish to return to the main menu?");
+            ButtonType btnYes = new ButtonType("Yes");
+            ButtonType btnNo = new ButtonType("No", ButtonData.CANCEL_CLOSE);
+            alert.getButtonTypes().setAll(btnYes, btnNo);
+            Optional<ButtonType> result = alert.showAndWait();
+        }
+    }
+
     public void keyRelease(KeyEvent event) {
-      if (event.getCode()== KeyCode.LEFT){
-      left = false;}
-      if (event.getCode()== KeyCode.RIGHT){
-      right = false;}
-      if (event.getCode()== KeyCode.DOWN){
-      down = false;}
-      if(event.getCode()==KeyCode.UP){
-      up = false;}   
-}
-    
-    
+        if (event.getCode() == KeyCode.LEFT) {
+            left = false;
+        }
+        if (event.getCode() == KeyCode.RIGHT) {
+            right = false;
+        }
+        if (event.getCode() == KeyCode.DOWN) {
+            down = false;
+        }
+        if (event.getCode() == KeyCode.UP) {
+            up = false;
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         approach.setCycleCount(Timeline.INDEFINITE);
         imgU.setLayoutY(-100);
         imgB.setTranslateY(40);
-    }    
-
-    private void move() { 
-           imgU.setTranslateY(imgU.getTranslateY()+h);
-           if(c(imgU, imgUser)){
-               if(list.get(0)== 1 && left == true){
-                   list.removeAll(list);
-                choose();
-                imgU.setTranslateY(-100);
-               }if(list.get(0)== 2 && up == true){
-                   list.removeAll(list);
-                choose();
-                imgU.setTranslateY(-100);
-               }if(list.get(0)== 3 && down == true){
-                   list.removeAll(list);
-                choose();
-                imgU.setTranslateY(-100);
-               }if(list.get(0)== 4 && right == true){
-                   list.removeAll(list);
-                choose();
-                imgU.setTranslateY(-100);
-               }}
-               if(c(imgU, imgB)){
-                   list.removeAll(list);
-                    choose();
-                    imgU.setTranslateY(-100);}            
     }
 
-    public void choose() {   
-    rand = ThreadLocalRandom.current().nextInt(1,4+1);
-    switch (rand){
-        case 1:
-    imgU.setLayoutX(70);
-    list.add(1);
-        break;
-        case 2:
-    imgU.setLayoutX(190);
-    list.add(2);
-    
-        break;
-        case 3:
-    imgU.setLayoutX(340);
-    list.add(3);
-    
-        break;
-        case 4:
-    imgU.setLayoutX(510);
-    list.add(4);
-    
-        break;
+    public void move() {
+        imgU.setTranslateY(imgU.getTranslateY() + h);
+        if (c(imgU, imgUser)) {
+            if (list.get(0) == 1 && left == true) {
+                list.removeAll(list);
+                choose();
+                imgU.setTranslateY(-100);
+                success++;
+                remain--;
+            }
+            if (list.get(0) == 2 && up == true) {
+                list.removeAll(list);
+                choose();
+                imgU.setTranslateY(-100);
+                success++;
+                remain--;
+            }
+            if (list.get(0) == 3 && down == true) {
+                list.removeAll(list);
+                choose();
+                imgU.setTranslateY(-100);
+                success++;
+                remain--;
+            }
+            if (list.get(0) == 4 && right == true) {
+                list.removeAll(list);
+                choose();
+                imgU.setTranslateY(-100);
+                success++;
+                remain--;
+            }
+        }
+        if (c(imgU, imgB)) {
+            remain --;
+            if(remain == 0){
+            approach.stop();
+            }
+            list.removeAll(list);
+            choose();
+            imgU.setTranslateY(-100);
+            if (h >= 4) {
+                h--;
+            }
+        }
     }
-     //***Get fonts from 1001fonts.com or whatever***
+
+    public void choose() {
+        if (success > 3 && h > 8) {
+            h++;
+            success = 0;
+        }
+        rand = ThreadLocalRandom.current().nextInt(1, 4 + 1);
+        switch (rand) {
+            case 1:
+                imgU.setLayoutX(70);
+                list.add(1);
+                break;
+            case 2:
+                imgU.setLayoutX(190);
+                list.add(2);
+
+                break;
+            case 3:
+                imgU.setLayoutX(340);
+                list.add(3);
+
+                break;
+            case 4:
+                imgU.setLayoutX(510);
+                list.add(4);
+
+                break;
+        }
+        //***Get fonts from 1001fonts.com or whatever***
     }
-    
+
     public boolean c(ImageView block1, ImageView block2) {
-    return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
+        return (block1.getBoundsInParent().intersects(block2.getBoundsInParent()));
     }
 
     private void top() {
-       list.removeAll(list);
-       choose();
-       imgU.setTranslateY(-100);
+        list.removeAll(list);
+        choose();
+        imgU.setTranslateY(-100);
     }
 }
